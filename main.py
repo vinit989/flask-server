3c54ff6c86448958381edbf645a56a12d1026f3a
DragonX <72137790+vinit989@users.noreply.github.com>
2024-02-27 10:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from flask import Flask, make_response
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/api')
-def home():
-    filename = 'hacky.yaml'
-    response = make_response(open(filename).read())
-    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename)
-    return response
+@app.route('/')
+def serve_file():
+    return send_file('hacky.yml', as_attachment=True)
 
-# This function is required for Vercel serverless function
-def handler(request, context):
-    from werkzeug.wrappers import Request, Response
-    with app.request_context(request):
-        response = app.full_dispatch_request()
-    return Response(response.data, status=response.status_code, headers=dict(response.headers))
-
-# This block allows running Flask app locally
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
